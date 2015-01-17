3fc8d91f18238af969973d5da11798b6bad32013
Jeremy Tan <150933@hkis.edu.hk>
2015-01-17 08:52:34 +0800

    Update
    Added Comments

--- BagelsOrderItem.java
+++ BagelsOrderItem.java
@@ -1,27 +1,27 @@
 
 public class BagelsOrderItem implements OrderItem{
 
-	private double price; 
+	private double price; 			//three fields are price, quantity, and cost. Made the private because they aren't essential for other classes
 	private int quantity;
 	private double cost;
 	
-	public BagelsOrderItem(double aPrice, int aQuantity)
+	public BagelsOrderItem(double aPrice, int aQuantity) //constructor that sets the price and quantity 
 	{
 		price=aPrice;
 		quantity=aQuantity;
 	}
 	
-	public double getPrice()
+	public double getPrice()			//getPrice method retrieves the price value 
 	{
 		return price;
 	}
 	
-	public int getQuantity()
+	public int getQuantity()	//getQuantity method retrieves the quantity value 
 	{
 		return quantity;
 	}
 	
-	public double getCost()
+	public double getCost()		//getCost method retrieves the cost value 
 	{
 		cost = price*quantity;
 	}
